c7db7391158d872e7f15c091cd662277a19c1822
Swapnil <Zandubam59@gmail.com>
2026-02-21 16:45:49 +0530

    mychange

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 <html>
 <body>
 
-<h1 style="color:blue;">A Orange Heading</h1>
+<h1 style="color:blue;">A Red Heading</h1>
 
-<p style="color:red;">A Green Heading.</p>
+<p style="color:red;">A orange Heading.</p>
 
 </body>
 </html>
